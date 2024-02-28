94670b4daec64a3e73852463ff4b995110ef7a00
HeroBuxx <me@herobuxx.me>
2024-02-28 20:48:27 +0700

    hana: module: start: Randomize message and make it fun
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- module/start.go
+++ module/start.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 type StartModule struct{}
@@ -13,14 +15,27 @@ type StartModule struct{}
 func (m *StartModule) Handle(ctx context.Context, b *bot.Bot, update *models.Update) {
 	message := update.Message.Text
 	if strings.HasPrefix(message, "/start") {
+		randomMessage := getRandomMessage()
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
-			Text:   "Hello there! I am Hana",
+			Text:   randomMessage,
 		})
-		fmt.Print("[LOG] Start module executed successfully")
+		fmt.Println("[LOG] Start module executed successfully")
 	}
 }
 
 func init() {
 	RegisterModule(&StartModule{})
+}
+
+func getRandomMessage() string {
+	messages := []string{
+		"Hello there! I am Hana",
+		"G'day mate! What's up?",
+		"Aye, howdy?",
+		"Greetings!",
+		"Yoyoyoyoyoyooo whaddup!",
+	}
+	rand.Seed(time.Now().UnixNano())
+	return messages[rand.Intn(len(messages))]
 }
