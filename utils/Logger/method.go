88b518a4b3b7041bdf2a8baf567d0bd34b7c46f2
HeroBuxx <me@herobuxx.me>
2024-04-29 18:50:43 +0700

    hana: utils: logger: Add function to send log to chat
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ utils/Logger/method.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"hanacore/config"
+	"time"
+)
+
+func ShowLog(module, senderID string) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+
+	logOutput := fmt.Sprintf("[%s] [%s] [LOG] %s Module executed successfully.", currentTime, senderID, module)
+	fmt.Println(logOutput)
+}
+
+func SendLog(ctx context.Context, b *bot.Bot, update *models.Update, senderID, module string) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	msgText := fmt.Sprintf("[%s] [%s] [LOG] %s Module executed successfully.", currentTime, senderID, module)
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: config.BotLogChat,
+		Text:   msgText,
+	})
+	fmt.Println("Sent bang")
+}
